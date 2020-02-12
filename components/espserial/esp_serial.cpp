dc0c7b16a65d3cb8736a7537430b2b8a6a4a3fbd
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-02-12 20:39:43 +0530

    cleaned the code with format.sh

--- components/espserial/esp_serial.cpp
+++ components/espserial/esp_serial.cpp
@@ -8,41 +8,42 @@
 
 #define led_pin GPIO_NUM_2  // ledp pin for debugging
 
+//////////////////////////////////////////////////////////////////////////////
 ros::NodeHandle nh;
+ros::Subscriber<geometry_msgs::Vector3> arm("ros_arm_control", &messageCb);
+//////////////////////////////////////////////////////////////////////////////
 
-void blinkDebugLed(){
+void blinkDebugLed()
+{
+    gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);
 
-  gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);
-  
-  gpio_set_level(led_pin, 1);
-  vTaskDelay(50);
-  
-  gpio_set_level(led_pin, 0);
-  vTaskDelay(50);
+    gpio_set_level(led_pin, 1);
+    vTaskDelay(50);
+
+    gpio_set_level(led_pin, 0);
+    vTaskDelay(50);
 }
 
-void messageCb(const geometry_msgs::Vector3& msg){    
-
+void messageCb(const geometry_msgs::Vector3 &msg)
+{
     ESP_LOGD("info", "%s", "new message from publisher");
 
     blinkDebugLed();
-    
+
     ESP_LOGD("angle 0", "%f", msg.x);
     ESP_LOGD("angle 1", "%f", msg.y);
     ESP_LOGD("angle 2", "%f", msg.z);
 
-    mcpwm_example_servo_control(msg.x, msg.y, msg.z);    
+    mcpwm_example_servo_control(msg.x, msg.y, msg.z);
 }
-
-ros::Subscriber<geometry_msgs::Vector3> arm("ros_arm_control", &messageCb);
 
 void rosserial_setup()
 {
-  nh.initNode();                  // Initialize ROS
-  nh.subscribe(arm);              // subscribe to topic
+    nh.initNode();                  // Initialize ROS
+    nh.subscribe(arm);              // subscribe to topic
 }
 
 void rosserial_spinonce()
 {
-  nh.spinOnce();
+    nh.spinOnce();
 }
--- components/ros_sra19/servo.c
+++ components/ros_sra19/servo.c
@@ -27,51 +27,51 @@ SOFTWARE.
 #include "servo.h"
 
 void mcpwm_example_gpio_initialize()
-    {
-        printf("initializing mcpwm servo control gpio......\n");
+{
+    printf("initializing mcpwm servo control gpio......\n");
 
-        //Set GPIO PINS as PWM0A, to which servo is connected
-        mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_PIN_1);    
-        mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, SERVO_PIN_2);
-        mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, SERVO_PIN_3);
-        printf("Configuring Initial Parameters of mcpwm......\n");
+    //Set GPIO PINS as PWM0A, to which servo is connected
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, SERVO_PIN_1);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, SERVO_PIN_2);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, SERVO_PIN_3);
+    printf("Configuring Initial Parameters of mcpwm......\n");
 
-        //Configure pwm
-        mcpwm_config_t pwm_config;
-        pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
-        pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
-        pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
-        pwm_config.counter_mode = MCPWM_UP_COUNTER;
-        pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    //Configure pwm
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
+    pwm_config.cmpr_a = 0;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
 
-        //Assign the configurations to MCPWM_UNIT
-        mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config); 
-        mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);     
-    }
+    //Assign the configurations to MCPWM_UNIT
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);
+}
 
 //Convert angles in degrees to angles in PWM for micro servo
 uint32_t micro_servo_per_degree_init(uint32_t degree_of_rotation)
-    {
-        uint32_t cal_pulsewidth_micro = 0;
-        cal_pulsewidth_micro = (MICRO_SERVO_MIN_PULSEWIDTH + 2*(((MICRO_SERVO_MAX_PULSEWIDTH - MICRO_SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
-        return cal_pulsewidth_micro;
-    }
+{
+    uint32_t cal_pulsewidth_micro = 0;
+    cal_pulsewidth_micro = (MICRO_SERVO_MIN_PULSEWIDTH + 2 * (((MICRO_SERVO_MAX_PULSEWIDTH - MICRO_SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+    return cal_pulsewidth_micro;
+}
 
 //Convert angles in degrees to angles in PWM for servo
 uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
-    {
-        uint32_t cal_pulsewidth = 0;
-        cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + 2*(((SERVO_MAX_PULSEWIDTH- SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
-        return cal_pulsewidth;
-    }
+{
+    uint32_t cal_pulsewidth = 0;
+    cal_pulsewidth = (SERVO_MIN_PULSEWIDTH + 2 * (((SERVO_MAX_PULSEWIDTH - SERVO_MIN_PULSEWIDTH) * (degree_of_rotation)) / (SERVO_MAX_DEGREE)));
+    return cal_pulsewidth;
+}
 
 //Write PWM values to all the Servos
-void mcpwm_example_servo_control(int theta1,int theta2,int theta3)
-    { 
-        PWM1 = servo_per_degree_init(theta1);
-        PWM2 = micro_servo_per_degree_init(theta2);
-        PWM3 = micro_servo_per_degree_init(theta3);
-        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, PWM1);
-        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, PWM2);
-        mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, PWM3);
-    }
+void mcpwm_example_servo_control(int theta1, int theta2, int theta3)
+{
+    PWM1 = servo_per_degree_init(theta1);
+    PWM2 = micro_servo_per_degree_init(theta2);
+    PWM3 = micro_servo_per_degree_init(theta3);
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, PWM1);
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, PWM2);
+    mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, PWM3);
+}
