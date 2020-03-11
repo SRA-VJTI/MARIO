d9b2e10de4dd20c4f73f22b0b42287887ef5bcdc
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-03-11 15:29:34 +0530

    Fixed servo sweep

--- servo_sweep/main/servo_sweep.c
+++ servo_sweep/main/servo_sweep.c
@@ -8,13 +8,13 @@ static void mcpwm_servo_control(void *arg)
     {	
     	for(int i = 0 ; i < 180 ; i++)
     	{
-    		servo_control(i,i,i);
+    		servo_control_sweep(i,i,i);
         	vTaskDelay(5);   
     	}
     	
     	for(int i = 180 ; i > 0; i--)
     	{
-    		servo_control(i,i,i);
+    		servo_control_sweep(i,i,i);
         	vTaskDelay(5);   
     	}
           
