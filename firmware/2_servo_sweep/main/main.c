20c7554886f2502514133302ce4967add0762ec9
ChinmayLonkar <chinmay20220@gmail.com>
2022-06-16 18:24:57 +0530

    Added OLED functionality in servo sweep

--- firmware/2_servo_sweep/main/main.c
+++ firmware/2_servo_sweep/main/main.c
@@ -1,5 +1,5 @@
-#include "servo.h"
 #include "freertos/task.h"
+#include "sra_board.h"
 
 #define TAG "MCPWM_SERVO_CONTROL"
 
@@ -43,12 +43,28 @@ servo_config servo_d = {
 	.gen = MCPWM_OPR_B,
 };
 
+#ifdef CONFIG_ENABLE_OLED
+// Declaring the required OLED struct
+u8g2_t oled_config;
+
+void update_oled()
+{
+	// Diplaying Servo A, Servo B, Servo C, Servo D values on OLED
+	while (1)
+	{
+		display_servo_values(read_servo(&servo_a), read_servo(&servo_b), read_servo(&servo_c), read_servo(&servo_d), &oled_config);
+	}
+}
+
+#endif
+
 static void mcpwm_servo_control(void *arg)
 {
 	enable_servo();
-	while(1)
+
+	while (1)
 	{
-		for(int i = 0 ; i < 180 ; i++)
+		for (int i = 0; i < 180; i++)
 		{
 			set_angle_servo(&servo_a, i);
 			set_angle_servo(&servo_b, i);
@@ -56,7 +72,8 @@ static void mcpwm_servo_control(void *arg)
 			set_angle_servo(&servo_d, i);
 			vTaskDelay(5);
 		}
-		for(int i = 180 ; i > 0; i--)
+
+		for (int i = 180; i > 0; i--)
 		{
 			set_angle_servo(&servo_a, i);
 			set_angle_servo(&servo_b, i);
@@ -69,7 +86,14 @@ static void mcpwm_servo_control(void *arg)
 
 void app_main()
 {
-	ESP_LOGD(TAG,"Testing servo motors\n");
-	xTaskCreate(mcpwm_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL);
-}
+	ESP_LOGD(TAG, "Testing servo motors\n");
 
+#ifdef CONFIG_ENABLE_OLED
+	ESP_ERROR_CHECK(init_oled(&oled_config));
+	display_mario_logo(&oled_config);
+	vTaskDelay(100);
+	xTaskCreatePinnedToCore(update_oled, "update oled", 4096, NULL, 5, NULL, 0);
+#endif
+
+	xTaskCreatePinnedToCore(mcpwm_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL, 1);
+}
