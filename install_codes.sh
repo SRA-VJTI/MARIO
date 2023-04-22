ab0ba1835a6578f9160d6686f249a544669b7955
Zain <beingzainsv@gmail.com>
2023-04-22 18:58:42 +0530

    Updated instructions

--- install_codes.sh
+++ install_codes.sh
@@ -28,7 +28,7 @@ function install_mambaforge() {
         export PATH="$HOME/mambaforge/bin:$PATH"
         # Initialize Mambaforge
         mamba init --all
-        echo "Mambaforge initialized. Please re-run the installation script for further configuration"
+        echo "$green Mambaforge initialized. ReOpen a new terminal, and Please re-run the installation script for further configuration"
         exit 0
     fi
 }
