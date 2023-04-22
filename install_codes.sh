6e3bc005cdc0c21d422ff1599841a5c6485e22c6
Zain <beingzainsv@gmail.com>
2023-04-22 07:40:51 +0000

    Added ROS noetic installation using Robostack

--- install_codes.sh
+++ install_codes.sh
@@ -1,61 +1,79 @@
-#!/bin/bash
-# Run it as root first and then without
-# sudo chmod +x ros_installation.sh
-# sudo ./ros_installation.sh && ./ros_installation.sh
-# After the file runs successfully, try running roscore in a new terminal.go to catkin_ws present in home and try running catkin_make.
+#!/usr/bin/env bash
+
 set -e
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+
 ros_distro='noetic'
-if [[ $(lsb_release -rs) == "18.04" ]]; then
-        ros_distro="melodic"
-fi
+
 echo $ros_distro
 pkg=ros-$ros_distro-desktop-full
+shell="${SHELL#${SHELL%/*}/}"
+user=$(whoami)
 
-
-function install(){
-    apt-get update
-    apt-get install curl
-    echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
-    curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
-    apt-get update
-    apt-get install ros-$ros_distro-desktop-full python3-pip ros-$ros_distro-effort-controllers ros-$ros_distro-rosserial-arduino
-    apt-get install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential net-tools
-    rosdep init
+function install_mambaforge() {
+    if command -v mamba &>/dev/null; then
+        echo "Mambaforge is already installed"
+    else
+        echo "Installing Mambaforge"
+        # Change the download link to the latest version
+        wget -q https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh -O mambaforge.sh
+        bash mambaforge.sh -b
+        rm mambaforge.sh
+        echo "Mambaforge installed"
+        echo "Initializing Mambaforge"
+        # Add Mambaforge to the PATH
+        export PATH="$HOME/mambaforge/bin:$PATH"
+        # Initialize Mambaforge
+        mamba init --all
+        echo "Mambaforge initialized"
+    fi
 }
 
-function configure(){
+function install() {
+    install_mambaforge
+    export CONDA_ALWAYS_YES="true"
+    conda install -y mamba -c conda-forge
+
+    mamba create -n ros_env python=3.9 -c conda-forge
+    conda activate ros_env && conda config --env --add channels conda-forge && conda config --env --add channels robostack-staging
+    conda config -n ros_env --env --remove channels defaults
+    mamba install -n ros_env -y ros-noetic-desktop-full ros-noetic-rosserial-python compilers cmake pkg-config make ninja catkin_tools rosdep
+    unset CONDA_ALWAYS_YES
+    conda activate ros_env && rosdep init && rosdep update
+}
+
+function configure() {
+
     set +e
-    dpkg -s $pkg &> /dev/null
-    if  [ $? -eq 0 ]; then
+    conda list -n ros_env $pkg &>/dev/null
+    if [ $? -eq 0 ]; then
         echo "$green Successfully installed $pkg $reset"
-    else
-        echo "$red $pkg is not installed. Run this script as root $reset"
         exit 1
+    else
+        echo "$red $pkg is not installed in the Conda environment. Installing. $reset"
+        install
     fi
-    /usr/bin/python3 -m pip install empy
-    echo "alias get_ros_noetic='source /opt/ros/$ros_distro/setup.bash'" >> $HOME/.bash_aliases
-    source $HOME/.bashrc
-    source /opt/ros/$ros_distro/setup.bash
-    rosdep update
+
+    conda activate ros_env && rosdep update
     cd $HOME
     mkdir -p $HOME/catkin_ws/src
     cd $HOME/catkin_ws/src
+
     if [[ ! -d "1_chatter_listener" ]]; then
         git clone --recursive https://github.com/SRA-VJTI/MARIO.git /tmp/ros_ws
         mv /tmp/ros_ws/1_* $HOME/catkin_ws/src
-	mv /tmp/ros_ws/2_* $HOME/catkin_ws/src
-	mv /tmp/ros_ws/3_* $HOME/catkin_ws/src
-	mv /tmp/ros_ws/4_* $HOME/catkin_ws/src
+        mv /tmp/ros_ws/2_* $HOME/catkin_ws/src
+        mv /tmp/ros_ws/3_* $HOME/catkin_ws/src
+        mv /tmp/ros_ws/4_* $HOME/catkin_ws/src
         if [[ ! -d "$HOME/ros_ws_firmware" ]]; then
             mkdir -p $HOME/ros_ws_firmware
             mv /tmp/ros_ws/firmware/* $HOME/ros_ws_firmware
             echo "${red}======================"
             echo "$green Ros Repository cloned newly and processed : ESP32 Codes $reset"
-        else 
+        else
             echo "${red}======================"
             echo "$green Already processed : ESP32 Codes $reset"
         fi
@@ -66,30 +84,15 @@ function configure(){
         echo "${red}======================"
         echo "$green Ros Repository already existed and processed $reset"
     fi
+
     cd ..
-    catkin_make
+    conda activate ros_env && catkin_make
     source $HOME/catkin_ws/devel/setup.bash
 }
 
-if [[ "$EUID" -eq 0 ]]; 
-then 
-    install
-    echo "${red}======================"
-    echo "${green}Please run without root once more" 1>&2 
-    echo "${red}======================"
-    exit
-else
-    configure
-    echo "${green}======================"
-    echo "${green}Installed & Configured"
-    echo "${green}======================"
-    exit
-fi
+configure
 
-
-
-
-
-
-
-
+echo "${green}======================"
+echo "${green}Installed & Configured"
+echo "${green}======================"
+exit
