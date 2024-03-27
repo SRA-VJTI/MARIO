8fc4d7e801828486ee35200a6cb56c83d4b0f3c1
Herculoxz <114341094+Herculoxz@users.noreply.github.com>
2024-03-28 00:26:38 +0530

    Update rviz.launch.py

--- 3_simulation_rviz/launch/rviz.launch.py
+++ 3_simulation_rviz/launch/rviz.launch.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#without GUI
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -14,7 +15,6 @@ import random
 def generate_launch_description():
     ####### DATA INPUT ##########
     urdf_file = 'manipulator.urdf'
-    #xacro_file = "box_bot.xacro"robot_description
     package_description = "simulation_rviz"
     ####### DATA INPUT END ##########
     config = os.path.join( get_package_share_directory('simulation_rviz'),
@@ -23,14 +23,7 @@ def generate_launch_description():
     )
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
     print("Fetching URDF ==>")
-
-    # joint_state_publisher = Node(
-    #         package="joint_state_publisher",
-    #         executable="joint_state_publisher"
-    #         )
-
-
-
+    
     # Robot State Publisher
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
@@ -40,7 +33,7 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True, '-r': 10, 'robot_description': ParameterValue(Command(['xacro ',robot_desc_path]), value_type=str)}],
         output="screen"
     )
-
+ 
     rviz_launch = Node(
             package='rviz2',
             namespace='',
@@ -49,49 +42,45 @@ def generate_launch_description():
             parameters=[{'use_sim_time' : True}],
             arguments=['-d' + os.path.join(get_package_share_directory(package_description), 'rviz', 'manipulator.rviz')]
         )
+     # This node is to publish static funtion values of base link to link 1
     tf2_link1 = Node(
             package='tf2_ros',
             namespace='',
             executable='static_transform_publisher',
             arguments=["-0.0030196", "0.046937", "0.0635", "0", "0", "0", "base_link", "link_1"]
         )
+     # This node is to publish static funtion values of link 1 to link 2
     tf2_link2 = Node(
             package='tf2_ros',
             namespace='',
             executable='static_transform_publisher',
             arguments=["0.00031511", "-0.0095653", "0.034407", "0", "0", "0", "link_1", "link_2"]
         )
-
+     # This node is to publish static funtion values of link 2 to link 3
     tf2_link3 = Node(
             package='tf2_ros',
             namespace='',
             executable='static_transform_publisher',
             arguments=["0.071531", "0.011279", "-0.0041348", "0", "0", "0", "link_2", "link_3"]
         )
-    
+     # This node is to publish static funtion values of link 3 to link claw_right
     tf2_claw_right = Node(
             package='tf2_ros',
             namespace='',
             executable='static_transform_publisher',
             arguments=["0.020925", "-0.013767", "-0.090331", "0", "0", "0", "link_3", "claw_right"]
         )
-    
+     # This node is to publish static funtion values of claw_right to claw_left
     tf2_claw_left = Node(
             package='tf2_ros',
             namespace='',
             executable='static_transform_publisher',
             arguments=["0.0091023", "0.0182709", "-0.090512", "0", "0", "0", "link_3", "claw_left"]
         )
-    # load_joint_position_controller = ExecuteProcess(
-    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'position_controllers'],
-    # output='screen'
-    # )
-    # create and return launch description object
+  
     print("==> Using Sim time\n")
     return LaunchDescription([  
-        # load_joint_position_controller,
         tf2_link1,tf2_link2,tf2_link3,tf2_claw_left,tf2_claw_right,
         robot_state_publisher_node,
         rviz_launch,
-        # joint_state_publisher,
     ])
